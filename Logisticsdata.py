a25a08947627c160007f4738136b3ada21d5db54
Jane-zzz-123 <2567696864@qq.com>
2026-04-08 18:59:57 +0800

    Add files via upload

--- Logisticsdata.py
+++ Logisticsdata.py
@@ -1972,7 +1972,7 @@ if missing_cols:
     st.error(f"缺少月度分析必要列：{missing_cols}，请检查数据列名！")
 else:
     # ---------------------- 【新增：剔除异常数据】 ✅ 修复这里 ----------------------
-    df_selected_clean = df_selected[df_selected["是否异常数据"] != "是"].copy()
+    df_selected_clean = df_selected[df_selected["是否为异常数据"] != "是"].copy()
     # ---------------------- 【新增1】：物流方式筛选器（核心新增） ----------------------
     st.markdown("### 筛选条件")  # 保留原标题，位置提前
     # 新增：控制筛选器列宽，界面更美观
